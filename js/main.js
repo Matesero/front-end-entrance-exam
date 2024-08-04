c8d751341396e6f779b64cbd2ad5a50f8e5479a2
Matesero <grishenkov05@mail.ru>
2024-08-04 22:05:10 +0700

    добавлен редактор для текста

--- js/main.js
+++ js/main.js
@@ -1,15 +1,48 @@
-const rippleText = document.querySelector('p');
+let texts = Array.from(document.querySelectorAll('p'));
+for (let text of document.querySelectorAll('h1')){
+    texts.push(text);
+}
+document.querySelectorAll('h1').forEach(text => {
+    texts.push(text);
+})
+document.querySelectorAll('h2').forEach(text => {
+    texts.push(text);
+})
 
-rippleText.addEventListener('mousedown', (event) => {
-    const x = event.clientX - rippleText.offsetLeft;
-    const y = event.clientY - rippleText.offsetTop;
+const editor = document.querySelector('.editor');
+const input = document.getElementById('input-text');
+const saveButton = document.getElementById('save-button');
+const closeButton = document.getElementById('close-button');
+texts.forEach(editableText => {
+    editableText.addEventListener('click', () => {
+        editor.classList.remove('hidden');
+        input.value = editableText.textContent;
+        if (input.value === " "){
+            input.value = "";
+        }
+        input.style.height = "30px";
+        editor.dataset.target = editableText.id;
+    });
+});
 
-    const ripple = document.createElement('span');
-    ripple.style.left = x + 'px';
-    ripple.style.top = y + 'px';
-    rippleText.appendChild(ripple);
+input.addEventListener('input', () =>{
+    const targetElement = document.getElementById(editor.dataset.target);
+    targetElement.textContent = input.value;
+});
 
-    setTimeout(() => {
-        ripple.remove();
-    }, 1000); // Удаляем волну через 1 секунду
+saveButton.addEventListener('click', () => {
+    const targetElement = document.getElementById(editor.dataset.target);
+
+    console.log(input.value.length)
+    if (input.value.length === 0){
+        targetElement.textContent = " ";
+    } else {
+        targetElement.textContent = input.value;
+    }
+    console.log(targetElement.textContent.length)
+    editor.classList.add('hidden');
+});
+
+closeButton.addEventListener('click', () => {
+    editor.classList.add('hidden');
 });
